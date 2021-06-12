71b8be43ba5ca1fba607b4a9404f222869235565
Utibeabasi Peter Umanah <utibeabasi@users-MacBook-Pro.local>
2021-06-12 04:14:02 +0100

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn frontend.wsgi.py
+web: gunicorn portfolio.wsgi
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r80%!8ccsh3ho=wryi36!e7ww#zbhz8za$4t!t!3xhaghue4$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['utibeumanah.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
